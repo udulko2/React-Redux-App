add303b21769c52eac2d4a1d7cca41bf4b14583b
udu-local <106012463+udulko2@users.noreply.github.com>
2023-04-18 20:45:29 +0300

    Search and load data with animation

--- src/components/actions/repos.js
+++ src/components/actions/repos.js
@@ -1,9 +1,13 @@
 import axios from "axios";
-import { setRepos } from "../../reducers/reposReducer";
+import { setIsFetching, setRepos } from "../../reducers/reposReducer";
 
 export const getRepos = (searchQuery = "stars:%3E1") => {
-  return async (dispath) => {
-    const response = await axios.get(`https://api.github.com/search/repositories?q={searchQuery}&sort=stars`)
-    dispath(setRepos(response.data))
+  if (searchQuery.trim().length === 0) {
+    searchQuery = "stars:%3E1"
+  }
+  return async (dispatch) => {
+    dispatch(setIsFetching(true))
+    const response = await axios.get(`https://api.github.com/search/repositories?q=${searchQuery}&sort=stars`)
+    dispatch(setRepos(response.data))
   }
 }
--- src/components/main/repo/Repo.jsx
+++ src/components/main/repo/Repo.jsx
@@ -11,7 +11,7 @@ const Repo = (props) => {
         <div className="repo-header-stars">Stars: {repo.stargazers_count}</div>
       </div>
       <div className="repo-last-commit">Last commit: {repo.updated_at}</div>
-      <a href={repo.html_url} className="repo-link">Link to repo: {repo.html_url}</a>
+      <div className="repo-link-wrapper">Link to repo: <a href={repo.html_url} className="repo-link">{repo.html_url}</a></div>
     </div>
   );
 };
